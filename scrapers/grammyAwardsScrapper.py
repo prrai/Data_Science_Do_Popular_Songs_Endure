1fc523e0824441d2c082e7774a424e5d771604e7
prrai <prrai@cs.stonybrook.edu>
2017-11-14 22:17:07 -0500

    Remove long files

--- scrapers/grammyAwardsScrapper.py
+++ scrapers/grammyAwardsScrapper.py
@@ -53,6 +53,38 @@ def populate():
     base_df.to_csv('{0}/{1}.csv'.format(path_final_csv, 'after_grammy_lifetime_achievement'), index=False)
 
 
+def wins_and_nominations():
+    home_page = r'grammys/artists/'
+    page = home_site + home_page
+    base_df = pd.read_csv('{0}/{1}.csv'.format(path_final_csv, 'after_grammy_lifetime_achievement'), encoding='latin-1')
+    base_df['Artist_grammy_nominations'] = ""
+    base_df['Artist_grammy_wins'] = ""
+    ctr = 1
+    for i, row in base_df.iterrows():
+        row_artist_tokens = row.Artist.lower().split()
+        print(i)
+        for j in range(1, len(row_artist_tokens) + 1):
+            slugged = '-'.join(row_artist_tokens[0:j])
+            try:
+                conn = urlopen(page + slugged)
+                html = conn.read()
+                soup = BeautifulSoup(html, "lxml")
+                table = soup.find_all('div', {'class': 'field-item even'})
+                base_df.loc[i, 'Artist_grammy_nominations'] = int(table[0].string)
+                base_df.loc[i, 'Artist_grammy_wins'] = int(table[1].string)
+                # print("Worked fine for: {}, wins={}, nominations={}, ctr= {}".format(slugged, int(table[1].string),
+                                                                                     # int(table[0].string), ctr))
+                ctr += 1
+                break
+            except:
+                # print("Failed for: {}".format(slugged))
+                base_df.loc[i, 'Artist_grammy_nominations'] = 0
+                base_df.loc[i, 'Artist_grammy_wins'] = 0
+
+    base_df.to_csv('{0}/{1}.csv'.format(path_final_csv, 'after_grammy_nominations_wins'), index=False)
+
+
 if __name__ == '__main__':
     # run()
-    populate()
+    # populate()
+    wins_and_nominations()
